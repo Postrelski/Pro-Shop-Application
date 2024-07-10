bfe432ae719d7e0f6d063216264622a1752e4379
Matthew Postrel <matthewpostrel@gmail.com>
2024-07-10 13:42:10 -0400

    Added Header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
-import React from "react";
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
 
 const App = () => {
-  return <h2>Welcome to Pro Shop</h2>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome To ProShop</h1>
+        </Container>
+      </main>
+    </>
+  );
 };
 
 export default App;
